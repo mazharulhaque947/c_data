e24b1c5d56947ed25f0b22b72c877adc17c0ed0c
mazharulhaque947 <mazharulhaque94721@gmail.com>
2025-09-16 04:00:40 +0600

    linklist1

---
+++ c_link_list.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+ typedef   struct nno
+{
+  int a;
+
+struct  nno *p;
+
+
+} ppt;
+
+ppt* fas( int a){
+
+ppt* b =  (ppt*) malloc(sizeof(ppt));
+b->a=a;
+b->p=NULL;
+
+return b;
+};
+
+
+void aad(ppt *s , int a){
+
+ppt *v ;
+v=s;
+while(v->p!=NULL){
+
+    v=v->p;
+
+
+
+}
+ppt *w=(ppt*)malloc(sizeof(ppt));
+v->p=w;
+w->a=a;
+w->p=NULL;
+
+
+
+
+}
+
+int main(){
+
+ppt *st,*o;
+st = fas(10);
+aad(st,5);
+o=st;
+o=o->p;
+//while(o->p!=NULL){
+printf("%d  ",o->a);
+
+//}
+
+
+
+
+return 0;
+}
+
+
